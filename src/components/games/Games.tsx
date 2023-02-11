d40d15f1ad0b17066cb3e4a9979ad358c2b4fd51
16Bogdan54 <Yunakov74@gmail.com>
2023-02-11 13:23:19 +0200

    card

--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import Card from "@mui/material/Card";
+import { CardContent, Typography } from "@mui/material";
 
 const Games = () => {
   const {
@@ -16,7 +18,28 @@ const Games = () => {
 
   console.log(games);
 
-  return <div>games</div>;
+  return (
+    <div>
+      <Card sx={{ maxWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div">
+            belent
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Games;
